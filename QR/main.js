3086fddd2c754236ddf7ccce5a3f421134008acb
Alok Shrestha <hello@aloks.com.np>
2023-07-09 18:57:29 +0545

    Add files via upload

--- QR/main.js
+++ QR/main.js
@@ -2,20 +2,21 @@ var qrAPIlink = "https://quickchart.io/qr?size=200&text=";
 // "https://api.qrserver.com/v1/create-qr-code/?size=300x300&data="
 var accounts = data;
 
-var getone = function (wtype, key, apply = false) {
-    var value = accounts[wtype][key];
+var getone = function (QRtype, key, apply = false) {
+    var value = accounts[QRtype][key];
     var button = document.createElement('button');
     button.innerHTML = key;
     button.classList = ["button"]
     button.onclick = function () {
         var qrData = value['qrdata'];
-        document.getElementById("image").src = qrAPIlink + encodeURIComponent(JSON.stringify(qrData)); document.getElementById("text").innerHTML = '<b>' + key + '</b><br/>' + (wtype == "Bank Transfer" ? qrData["accountName"] + '<br/>' + qrData["accountNumber"] + '<br/>' : '');
+        document.getElementById("image").src = qrAPIlink + encodeURIComponent((QRtype == "Bank Transfer" ? JSON.stringify(qrData) : qrData));
+        document.getElementById("text").innerHTML = '<b>' + key + '</b><br/>' + (QRtype == "Bank Transfer" ? qrData["accountName"] + '<br/>' + qrData["accountNumber"] + '<br/>' : '');
         // alert(JSON.stringify(value)); return false; JSON.stringify(qrData)
     };
     if (apply == true) {
         var qrData = value['qrdata'];
         document.getElementById("image").src = qrAPIlink + encodeURIComponent(JSON.stringify(qrData));
-        document.getElementById("text").innerHTML = '<b>' + key + '</b><br/>' + (wtype == "Bank Transfer" ? qrData["accountName"] + '<br/>' + qrData["accountNumber"] + '<br/>' : JSON.stringify(qrData));
+        document.getElementById("text").innerHTML = '<b>' + key + '</b><br/>' + (QRtype == "Bank Transfer" ? qrData["accountName"] + '<br/>' + qrData["accountNumber"] + '<br/>' : JSON.stringify(qrData));
     }
     return button;
 };
@@ -23,12 +24,12 @@ var getone = function (wtype, key, apply = false) {
 getone('Bank Transfer', 'NIC Asia Bank (preferred)', true);
 
 var foo = function () {
-    for (const [wtype, wdata] of Object.entries(accounts)) {
+    for (const [QRtype, QRdata] of Object.entries(accounts)) {
         var wdiv = document.createElement('h2');
-        wdiv.innerHTML = wtype;
+        wdiv.innerHTML = QRtype;
         document.getElementById('foobutton').appendChild(wdiv);
-        for (const [key, value] of Object.entries(wdata)) {
-            var button = getone(wtype, key);
+        for (const [key, value] of Object.entries(QRdata)) {
+            var button = getone(QRtype, key);
             document.getElementById('foobutton').appendChild(button);
             // document.body.appendChild(button);
         }
